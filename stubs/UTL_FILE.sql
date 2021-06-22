a9ab8a57371d9c921daf14b7816a73dd6f30e2d2
Orellabac <orellabac@gmail.com>
2021-06-22 13:13:36 -0600

    an example implementation for some UTL_FILE methods

--- stubs/UTL_FILE.sql
+++ stubs/UTL_FILE.sql
@@ -1,11 +1,87 @@
+
 CREATE SCHEMA UTL_FILE;
 
-CREATE OR REPLACE PROCEDURE UTL_FILE.FCLOSE()
+/** To test some methods you can use this stored procedure */
+
+CREATE OR REPLACE PROCEDURE TEST_UTL_FILE()  RETURNS STRING LANGUAGE JAVASCRIPT
+AS
+$$
+//region Helpers
+var _RS;
+var fixBind = arg => arg instanceof Object ? JSON.stringify(arg) : arg === undefined ? null : arg;
+var EXEC = function(stmt,args) { _RS = snowflake.execute({sqlText:stmt,binds:(args||[]).map(fixBind)});}
+var INTO = function() { return _RS.next() && new Array(_RS.getColumnCount()).fill(null).map((x,i)=>_RS.getColumnValue(i+1)); }
+//endregion
+
+EXEC(`call UTL_FILE.FOPEN('c:/temp','file1','w',10000)`);
+var [handle] = INTO();
+
+EXEC(`call UTL_FILE.PUT_LINE(?,'Hello World')`,[handle]);
+EXEC(`call UTL_FILE.PUT_LINE(?,'Salut, monde!')`,[handle]);
+EXEC(`call UTL_FILE.PUT_LINE(?,'Hola mundo!')`,[handle]);
+EXEC(`call UTL_FILE.FCLOSE(?)`,[handle]);
+  
+// This example will then allow you to download that file
+// to do that please log into snowflake using the snowsql CLI
+// once connected run: get @~/c:/temp/file1_0_0_0.csv.gz file://.;
+// That will download the file into the current directory.
+// Exit from snowsql with !exit and from the command prompt you can run
+// gunzip file1_0_0_0.csv.gz
+$$;
+
+CREATE OR REPLACE PROCEDURE UTL_FILE.FOPEN(LOCATION VARCHAR,FILENAME VARCHAR,OPEN_MODE VARCHAR,MAX_LINESIZE FLOAT)
+RETURNS VARCHAR
+LANGUAGE JAVASCRIPT
+AS $$
+/**
+ * This code is just an example of how this functionality can be implemented.
+ * There are some optimizations that can be considered or performance implications.
+ * This implementation uses two tables. FOPEN_TABLES which holds a list of all the opened tables, 
+ * and FOPEN_TABLES_LINES which holds all the lines that have been sent to the file using the PUT_LINE calls.
+ * When the fclose is called, all lines send to the file will be collected and sent to and stage file.
+ * We recommend using *nix style back slash for paths. 
+ * To list the generated files you can run LIST @~; to list all the files.
+ * To download them, we recommend using the snowsql CLI and once connected you can run: 
+ *  get @~/c:/temp/file1_0_0_0.csv.gz file://.;
+ * That will download the file into the current directory.
+ * Exit from snowsql with !exit and from the command prompt you can run
+ * gunzip file1_0_0_0.csv.gz
+ */
+
+  snowflake.execute({sqlText:`
+  CREATE TABLE IF NOT EXISTS FOPEN_TABLES 
+  (
+    FHANDLE VARCHAR, 
+    LOCATION VARCHAR,
+    FILENAME VARCHAR,
+    OPEN_MODE VARCHAR,
+    MAX_LINESIZE NUMBER
+  );`});
+
+  snowflake.execute({sqlText:`
+  CREATE TABLE IF NOT EXISTS FOPEN_TABLES_LINES 
+  (
+    SEQ    NUMBER AUTOINCREMENT,
+    FHANDLE VARCHAR, 
+    LINE    VARCHAR
+  );
+  `});
+  var rsId = snowflake.execute({sqlText:'select UUID_STRING() as key'});
+  rsId.next();
+  var key = rsId['KEY'];
+  snowflake.execute({sqlText:`INSERT INTO FOPEN_TABLES(FHANDLE, LOCATION, FILENAME, OPEN_MODE, MAX_LINESIZE) VALUES(?,?,?,?,?)  `,binds:[key, LOCATION, FILENAME, OPEN_MODE, MAX_LINESIZE]});
+  return JSON.stringify({loc:LOCATION, name: FILENAME, handle: key});  
+$$;
+
+
+CREATE OR REPLACE PROCEDURE UTL_FILE.FCLOSE(FILE VARCHAR)
 RETURNS STRING
 LANGUAGE JAVASCRIPT
 AS $$
-/** add some code here **/
-$$
+  FILE = JSON.parse(FILE);
+  snowflake.execute({sqlText:`COPY INTO @~/${FILE.loc}/${FILE.name} FROM (SELECT LINE FROM FOPEN_TABLES_LINES WHERE FHANDLE = ? ORDER BY SEQ);`, binds:[FILE.handle]});
+  snowflake.execute({sqlText:`DELETE FROM FOPEN_TABLES_LINES WHERE FHANDLE = ?`, binds:[FILE.handle]});
+$$;
 
 CREATE OR REPLACE PROCEDURE UTL_FILE.FCLOSE_ALL(SRC_FILENAME VARCHAR,DEST_LOCATION VARCHAR,DEST_FILENAME VARCHAR,START_LINE NUMBER,END_LINE NUMBER)
 RETURNS STRING
@@ -126,12 +202,21 @@ AS $$
 /** add some code here **/
 $$
 
-CREATE OR REPLACE PROCEDURE UTL_FILE.PUT_LINE(FILE VARIANT,BUFFER VARCHAR,AUTOFLUSH BOOLEAN)
+CREATE OR REPLACE PROCEDURE UTL_FILE.PUT_LINE(FILE VARCHAR,BUFFER VARCHAR)
 RETURNS STRING
 LANGUAGE JAVASCRIPT
 AS $$
-/** add some code here **/
-$$
+  FILE = JSON.parse(FILE);
+  snowflake.execute({sqlText:`INSERT INTO FOPEN_TABLES_LINES(FHANDLE,LINE) VALUES(?,?)`, binds:[FILE.handle,BUFFER]});
+$$;  
+  
+CREATE OR REPLACE PROCEDURE UTL_FILE.PUT_LINE(FILE VARCHAR,BUFFER VARCHAR,AUTOFLUSH BOOLEAN)
+RETURNS STRING
+LANGUAGE JAVASCRIPT
+AS $$
+  FILE = JSON.parse(FILE);
+  snowflake.execute({sqlText:`INSERT INTO FOPEN_TABLES_LINES(FHANDLE,LINE) VALUES(?,?)`, binds:[FILE.handle,BUFFER]);
+$$;
 
 CREATE OR REPLACE PROCEDURE UTL_FILE.PUT_LINE_NCHAR(FILE VARIANT,BUFFER NVARCHAR2)
 RETURNS STRING
